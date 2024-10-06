d01d9d90cee482a83ebb2477c844883fa31ab311
FerdiNP <ferdinaufal6809@gmail.com>
2024-10-06 12:01:37 +0700

    login main

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,20 +1,26 @@
+import '../pages/modules/login/views/main_login.dart';
 import 'package:get/get.dart';
 
-import '../modules/home/bindings/home_binding.dart';
-import '../modules/home/views/home_view.dart';
+import '../pages/modules/home/bindings/home_binding.dart';
+import '../pages/modules/home/views/home_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.MAINLOGIN;
 
   static final routes = [
+    GetPage(
+        name: _Paths.MAINLOGIN,
+        page: () => MainLogin()
+    ),
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
     ),
+
   ];
 }
